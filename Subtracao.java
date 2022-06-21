8b71f11c9360703aaa970fb7570f4cbe50e2ff68
MariaRafaela1 <mrs18@aluno.ifal.edu.br>
2022-06-21 13:31:42 +0000

    Adicionando Subtracao

---
+++ Subtracao.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class Subtracao implements HttpHandler{
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            String[] partes = exchange.getRequestURI().getPath().split("/");
+            float n1 = Float.parseFloat(partes[2]);
+            float n2 = Float.parseFloat(partes[3]);
+            float subtracao = n1-n2;
+                byte[] resposta = Float.toString(subtracao).getBytes();  
+                exchange.sendResponseHeaders(200, resposta.length);
+                exchange.getResponseBody().write(resposta);
+                return; 
+    
+        } finally {
+            exchange.close();
+        }
+    }
+}
