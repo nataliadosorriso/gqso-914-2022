240f6aae02e8144e0d7e724586ab66f81ef72def
nataliadosorriso <nataliadosorriso@gmail.com>
2022-07-25 11:42:43 -0300

    Corrigindo Adição

--- api/src/main/java/com/nataliadosorriso/ifal/calcapi/Adicao.java
+++ api/src/main/java/com/nataliadosorriso/ifal/calcapi/Adicao.java
@@ -5,12 +5,15 @@ import io.jooby.annotations.Path;
 import io.jooby.annotations.PathParam;
 import io.jooby.exception.BadRequestException;
 
-@Path("/adicao/{n1}/{n2}")
+@Path("/adicao/{n1}?/{n2}?")
 public class Adicao {
 
     @GET
     public double adicao(@PathParam("n1") String n1, @PathParam("n2") String n2) {
         try {
+            if (n1 == null || n2 == null) {
+                throw new BadRequestException("Parâmetros insuficientes");
+            }
             return Double.parseDouble(n1) + Double.parseDouble(n2);
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Parâmetros inválidos: " + n1+ ", " + n2);
