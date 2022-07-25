5b55420949329598771db90ca81da5b1cb74b480
nataliadosorriso <nataliadosorriso@gmail.com>
2022-07-25 11:34:27 -0300

    a

---
+++ api/src/test/java/com/nataliadosorriso/ifal/calcapi/AdicaoTest.java
@@ -0,0 +1,37 @@
+package com.nataliadosorriso.ifal.calcapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import io.jooby.exception.BadRequestException;
+
+public class AdicaoTest {
+    @Test
+    public void adicao() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/adicao/2/4", rsp -> {
+            assertEquals(6.0, rsp.value());
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void adicao_operadorString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/adicao/a/a", rsp -> {});
+        });
+    }
+
+    @Test
+    public void adicao_semOperador() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/adicao/2", rsp -> {});
+        });
+    }
+}
