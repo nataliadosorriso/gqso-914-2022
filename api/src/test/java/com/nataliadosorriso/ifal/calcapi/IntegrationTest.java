1784b7b54f727caae09df19294c87815de80a3a8
Natália Lara <nataliadosorriso@gmail.com>
2022-09-02 11:23:18 +0000

    testes de integração

--- api/src/test/java/com/nataliadosorriso/ifal/calcapi/IntegrationTest.java
+++ api/src/test/java/com/nataliadosorriso/ifal/calcapi/IntegrationTest.java
@@ -27,4 +27,102 @@ public class IntegrationTest {
       assertEquals(StatusCode.OK.value(), rsp.code());
     }
   }
+
+  //adição
+
+  @Test
+  public void adicao(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/adicao/8/5")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals("13.0", rsp.body().string());
+      assertEquals(StatusCode.OK.value(), rsp.code());
+    }
+  }
+  @Test
+  public void adicao_erro(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/adicao/2/a")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals(StatusCode.BAD_REQUEST.value(), rsp.code());
+    }
+  }
+
+  //subtracao
+
+  @Test
+  public void subtracao(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/subtracao/8/5")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals("3.0", rsp.body().string());
+      assertEquals(StatusCode.OK.value(), rsp.code());
+    }
+  }
+  @Test
+  public void subtracao_erro(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/subtracao/2/a")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals(StatusCode.BAD_REQUEST.value(), rsp.code());
+    }
+  }
+
+  //multiplicação
+
+  @Test
+  public void multiplicacao(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/multiplicacao/8/5")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals("40.0", rsp.body().string());
+      assertEquals(StatusCode.OK.value(), rsp.code());
+    }
+  }
+  @Test
+  public void multiplicacao_erro(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/multiplicacao/2/a")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals(StatusCode.BAD_REQUEST.value(), rsp.code());
+    }
+  }
+
+  //divisão
+
+  @Test
+  public void divisao(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/divisao/8/2")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals("4.0", rsp.body().string());
+      assertEquals(StatusCode.OK.value(), rsp.code());
+    }
+  }
+  @Test
+  public void divisao_erro(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/divisao/2/a")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals(StatusCode.BAD_REQUEST.value(), rsp.code());
+    }
+  }
+
+
 }
